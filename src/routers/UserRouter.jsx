57b7c18e8f307beb470d4d3bf955dac5f96091ab
inhooinu <inhooinu@gmail.com>
2025-03-07 11:38:04 +0900

    feat: add router configuration for transfer pages (#69)

--- src/routers/UserRouter.jsx
+++ src/routers/UserRouter.jsx
@@ -1,6 +1,28 @@
+import TransferRecipientPage from "../pages/UserPage/TransferRecipientPage";
+import TransferAmountPage from "../pages/UserPage/TransferAmountPage";
+import TransferConfirmPage from "../pages/UserPage/TransferConfirmPage";
+
 const UserRouter = {
   path: "/mypage",
-  children: [],
+  children: [
+    {
+      path: "transfer",
+      children: [
+        {
+          path: "recipient",
+          element: <TransferRecipientPage />, // 계좌번호 및 메모 입력 페이지
+        },
+        {
+          path: "amount",
+          element: <TransferAmountPage />, // 금액 입력 페이지
+        },
+        {
+          path: "confirm",
+          element: <TransferConfirmPage />, // 최종 확인 페이지
+        },
+      ],
+    },
+  ],
 };
 
 export default UserRouter;
